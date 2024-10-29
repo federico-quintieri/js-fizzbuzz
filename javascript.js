26d9926f3b517651ef48ef7266630ac7f25a4cef
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-10-29 13:45:38 +0100

    feat:Output message easier to understand

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@ let varStampa;
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    varStampa = "FizzBuzz";
+    varStampa = `La ${i} è multipla di 3 e di 5 quindi: 'FizzBuzz'`;
   } else if (i % 3 === 0) {
-    varStampa = "Fizz";
+    varStampa = `La ${i} è multipla di 3 quindi: 'Fizz'`;
   } else if (i % 5 === 0) {
-    varStampa = "Buzz";
-  } else varStampa = i;
+    varStampa = `La ${i} è multipla di 5 quindi: 'Buzz'`;
+  } else varStampa = `La i non è multipla nè di 3 nè di 5 quindi: ${i}`;
 
   console.log(varStampa);
 }
