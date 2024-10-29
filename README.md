34121e9c411fa98809883498efea73eed73acab6
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-10-29 13:57:19 +0100

    feat:custom message/numbers

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ INPUT
     Nessuno
 
 LOGICA
-    Ciclo for + if 
+    
+    Dobbiamo fare un ciclo con 100 iterazioni da 1 a 100.
+    
+    Per calcolare se un numero è multiplo di un'altro dobbiamo controllare che il suo resto diviso per questo numero sia 0.
+    
+    Partiamo controllando che sia divisibile per entrambi i numeri così evitiamo ripetizioni.
+
+    Modifichiamo il messaggio output in ogni input tramite una variabile globale.
+
+    Usiamo delle variabili constanti globali per poter customizzare messaggi e numeri da analizzare.
--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
+const firstNum = 3,
+  secondNum = 5;
+
+const firstMessage = "Fizz",
+  secondMessage = "Buzz";
+
 let varStampa;
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    varStampa = `La ${i} è multipla di 3 e di 5 quindi: 'FizzBuzz'`;
-  } else if (i % 3 === 0) {
-    varStampa = `La ${i} è multipla di 3 quindi: 'Fizz'`;
-  } else if (i % 5 === 0) {
-    varStampa = `La ${i} è multipla di 5 quindi: 'Buzz'`;
-  } else varStampa = `La i non è multipla nè di 3 nè di 5 quindi: ${i}`;
+  if (i % firstNum === 0 && i % secondNum === 0) {
+    varStampa = `${i}. La ${i} è multipla di ${firstNum} e di ${secondNum} quindi: ${
+      firstMessage + secondMessage
+    }`;
+  } else if (i % firstNum === 0) {
+    varStampa = `${i}. La ${i} è multipla di ${firstNum} quindi: ${firstMessage}`;
+  } else if (i % secondNum === 0) {
+    varStampa = `${i}. La ${i} è multipla di ${secondNum} quindi: ${secondMessage}`;
+  } else
+    varStampa = `${i}. La i non è multipla nè di ${firstNum} nè di ${secondNum} quindi: ${i}`;
 
   console.log(varStampa);
 }
